74d71383ef56be9f87637c1218222bf1356c0d60
tinannan <yashenkoalevtina@gmail.com>
2024-09-10 00:55:22 +0300

    Revert "change text button event handler changed"
    This reverts commit b0aebda841f128e61b4c1e37a311e90e79503bdc.

--- app/src/main/java/com/example/lab1madt/MainActivity.java
+++ app/src/main/java/com/example/lab1madt/MainActivity.java
@@ -3,9 +3,9 @@ package com.example.lab1madt;
 import android.annotation.SuppressLint;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.View;
 import android.widget.TextView;
-import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,9 +15,6 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
-    TextView tvChangeText;
-    Button btnChangeText;
-    @SuppressLint("SetTextI18n")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,22 +25,15 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-        this.tvChangeText = findViewById(R.id.tvChangeText);
-        this.btnChangeText = findViewById(R.id.button1);
-        this.btnChangeText.setOnClickListener(view ->
-                tvChangeText.setText("You have clicked the button"));
-
     }
 
+    @SuppressLint("SetTextI18n")
+    public void onBtnChangeTextClick(View view) {
+        TextView tvChangeText = findViewById(R.id.tvChangeText);
+        tvChangeText.setText("A small piece of code in most general-purpose programming languages, this program is used to illustrate a language's basic syntax.");
+        tvChangeText.setGravity(Gravity.CENTER);
+    }
 
-
-//    @SuppressLint("SetTextI18n")
-//    public void onBtnChangeTextClick(View view) {
-//        TextView tvChangeText = findViewById(R.id.tvChangeText);
-//        tvChangeText.setText("A small piece of code in most general-purpose programming languages, this program is used to illustrate a language's basic syntax.");
-//        tvChangeText.setGravity(Gravity.CENTER);
-//    }
-//
     public void onBtnChangeTextColourClick(View view) {
         TextView tvChangeText = findViewById(R.id.tvChangeText);
         tvChangeText.setTextColor(Color.parseColor("#A020F0"));
